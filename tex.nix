53d5d7151cd07d85d1da42ba5e7dfd58dd11fe7a
Garrett George <45802549+garrettmichaelgeorge@users.noreply.github.com>
2024-05-08 13:14:13 -0500

    refactor: extract tex.nix

---
+++ tex.nix
@@ -0,0 +1,11 @@
+{ pkgs }:
+
+let
+  texlivePkgs = { inherit (pkgs.texlive) scheme-medium latex-bin latexmk; };
+  cpanPkgs = {
+    inherit (pkgs.texlive)
+      bookmark enumitem environ etoolbox fontawesome5 fontspec hyperref ifmtarg
+      parskip ragged2e roboto setspace sourcesanspro tcolorbox tikzfill
+      unicode-math xcolor xifthen xstring;
+  };
+in pkgs.texlive.combine texlivePkgs // cpanPkgs
